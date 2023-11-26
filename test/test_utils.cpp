303a8efe8c3746f25f1c9cc993d7b22952686ea4
Koukyosyumei <koukyosyumei@hotmail.com>
2023-11-26 16:29:41 +0900

    implement basic tests

---
+++ test/test_utils.cpp
@@ -0,0 +1,58 @@
+#include "gtest/gtest.h"
+#include "../libgymbo/utils.h"  // Replace with the actual header file name
+
+// Test Linkedlist push function
+TEST(LinkedlistTest, Push) {
+  Linkedlist<int> list;
+  list.push(1);
+  list.push(2);
+  list.push(3);
+
+  // Check the length of the linked list
+  EXPECT_EQ(list.len(), 3);
+
+  // Check the elements
+  EXPECT_EQ(*list.back(), 3);
+}
+
+// Test Linkedlist pop function
+TEST(LinkedlistTest, Pop) {
+  Linkedlist<int> list;
+  list.push(1);
+  list.push(2);
+  list.push(3);
+
+  // Check the length before popping
+  EXPECT_EQ(list.len(), 3);
+
+  // Pop an element
+  list.pop();
+
+  // Check the length after popping
+  EXPECT_EQ(list.len(), 2);
+
+  // Check the elements
+  EXPECT_EQ(*list.back(), 2);
+}
+
+// Test Linkedlist back function
+TEST(LinkedlistTest, Back) {
+  Linkedlist<int> list;
+  list.push(1);
+  list.push(2);
+  list.push(3);
+
+  // Check the element at the back
+  EXPECT_EQ(*list.back(), 3);
+}
+
+// Test Linkedlist len function
+TEST(LinkedlistTest, Len) {
+  Linkedlist<int> list;
+  list.push(1);
+  list.push(2);
+  list.push(3);
+
+  // Check the length of the linked list
+  EXPECT_EQ(list.len(), 3);
+}
