7ac6d6d1271fe8b4c5ccc8920f6615a8d1a06c7b
Koukyosyumei <koukyosyumei@hotmail.com>
2023-11-27 23:21:14 +0900

    clangformat

--- libgymbo/gd.h
+++ libgymbo/gd.h
@@ -23,121 +23,126 @@ namespace gymbo {
  * constraints are satisfied or a maximum number of epochs is reached.
  */
 struct GDOptimizer {
+    int num_epochs, seed;
+    float lr, eps, param_low, param_high;
+    bool sign_grad;
 
-  int num_epochs, seed;
-  float lr, eps, param_low, param_high;
-  bool sign_grad;
+    int num_used_itr;
 
-  int num_used_itr;
+    /**
+     * @brief Constructor for GDOptimizer.
+     *
+     * @param num_epochs Maximum number of optimization epochs (default: 100).
+     * @param lr Learning rate for gradient descent (default: 1).
+     * @param param_low Lower bound for parameter initialization (default: -10).
+     * @param param_high Upper bound for parameter initialization (default: 10).
+     * @param sign_grad If true, use sign gradient descent. Otherwise, use
+     * standard gradient descent. (default true).
+     * @param seed Random seed for initializing parameter values (default: 42).
+     */
+    GDOptimizer(int num_epochs = 100, float lr = 1.0, float eps = 1.0,
+                float param_low = -10.0, float param_high = 10.0,
+                bool sign_grad = true, int seed = 42)
+        : num_epochs(num_epochs),
+          lr(lr),
+          eps(eps),
+          param_low(param_low),
+          param_high(param_high),
+          sign_grad(sign_grad),
+          seed(seed),
+          num_used_itr(0) {}
 
-  /**
-   * @brief Constructor for GDOptimizer.
-   *
-   * @param num_epochs Maximum number of optimization epochs (default: 100).
-   * @param lr Learning rate for gradient descent (default: 1).
-   * @param param_low Lower bound for parameter initialization (default: -10).
-   * @param param_high Upper bound for parameter initialization (default: 10).
-   * @param sign_grad If true, use sign gradient descent. Otherwise, use
-   * standard gradient descent. (default true).
-   * @param seed Random seed for initializing parameter values (default: 42).
-   */
-  GDOptimizer(int num_epochs = 100, float lr = 1.0, float eps = 1.0, float param_low = -10.0,
-              float param_high = 10.0, bool sign_grad = true, int seed = 42)
-      : num_epochs(num_epochs), lr(lr), eps(eps), param_low(param_low),
-        param_high(param_high), sign_grad(sign_grad), seed(seed),
-        num_used_itr(0) {}
-
-  /**
-   * @brief Evaluate if path constraints are satisfied for given parameters.
-   *
-   * This function checks if the given path constraints are satisfied
-   * (non-positive) for the provided parameter values.
-   *
-   * @param path_constraints Vector of symbolic path constraints.
-   * @param params Map of parameter values.
-   * @return `true` if all constraints are satisfied; otherwise, `false`.
-   */
-  bool eval(std::vector<Sym> &path_constraints,
-            std::unordered_map<int, float> params) {
-    bool result = true;
-    for (int i = 0; i < path_constraints.size(); i++) {
-      result = result && (path_constraints[i].eval(params, eps) <= 0.0f);
-    }
-    return result;
-  }
-
-  /**
-   * @brief Solve path constraints using gradient descent optimization.
-   *
-   * This function attempts to find parameter values that satisfy the given path
-   * constraints by using gradient descent optimization. It iteratively updates
-   * the parameters until the constraints are satisfied or the maximum number of
-   * epochs is reached.
-   *
-   * @param path_constraints Vector of symbolic path constraints.
-   * @param params Map of parameter values (will be modified during
-   * optimization).
-   * @param is_init_params_const Flag indicating whether initial parameter
-   * values are constant.
-   * @return `true` if the constraints are satisfied after optimization;
-   * otherwise, `false`.
-   */
-  bool solve(std::vector<Sym> &path_constraints,
-             std::unordered_map<int, float> &params,
-             bool is_init_params_const = true) {
-    if (path_constraints.size() == 0) {
-      return true;
-    }
-
-    std::mt19937 gen(seed);
-    std::uniform_int_distribution<> dist(param_low, param_high);
-
-    std::unordered_map<int, bool> is_const;
-    std::unordered_set<int> unique_var_ids;
-
-    for (int i = 0; i < path_constraints.size(); i++) {
-      path_constraints[i].gather_var_ids(unique_var_ids);
-    }
-
-    for (int i : unique_var_ids) {
-      if (params.find(i) == params.end()) {
-        params.emplace(std::make_pair(i, dist(gen)));
-        is_const.emplace(std::make_pair(i, false));
-      } else {
-        is_const.emplace(std::make_pair(i, is_init_params_const));
-      }
-    }
-
-    int itr = 0;
-    bool is_sat = eval(path_constraints, params);
-
-    while ((!is_sat) && (itr < num_epochs)) {
-      Grad grads = Grad({});
-      for (int i = 0; i < path_constraints.size(); i++) {
-        if (path_constraints[i].eval(params, eps) > 0.0f) {
-          grads = grads + path_constraints[i].grad(params,eps);
+    /**
+     * @brief Evaluate if path constraints are satisfied for given parameters.
+     *
+     * This function checks if the given path constraints are satisfied
+     * (non-positive) for the provided parameter values.
+     *
+     * @param path_constraints Vector of symbolic path constraints.
+     * @param params Map of parameter values.
+     * @return `true` if all constraints are satisfied; otherwise, `false`.
+     */
+    bool eval(std::vector<Sym> &path_constraints,
+              std::unordered_map<int, float> params) {
+        bool result = true;
+        for (int i = 0; i < path_constraints.size(); i++) {
+            result = result && (path_constraints[i].eval(params, eps) <= 0.0f);
         }
-      }
-      for (auto &g : grads.val) {
-        if (!is_const.at(g.first)) {
-          if (!sign_grad) {
-            params.at(g.first) -= lr * g.second;
-          } else {
-            float sign = 0.0f;
-            if (g.second > 0.0f) {
-              sign = 1.0;
-            } else if (g.second < 0.0f) {
-              sign = -1.0f;
+        return result;
+    }
+
+    /**
+     * @brief Solve path constraints using gradient descent optimization.
+     *
+     * This function attempts to find parameter values that satisfy the given
+     * path constraints by using gradient descent optimization. It iteratively
+     * updates the parameters until the constraints are satisfied or the maximum
+     * number of epochs is reached.
+     *
+     * @param path_constraints Vector of symbolic path constraints.
+     * @param params Map of parameter values (will be modified during
+     * optimization).
+     * @param is_init_params_const Flag indicating whether initial parameter
+     * values are constant.
+     * @return `true` if the constraints are satisfied after optimization;
+     * otherwise, `false`.
+     */
+    bool solve(std::vector<Sym> &path_constraints,
+               std::unordered_map<int, float> &params,
+               bool is_init_params_const = true) {
+        if (path_constraints.size() == 0) {
+            return true;
+        }
+
+        std::mt19937 gen(seed);
+        std::uniform_int_distribution<> dist(param_low, param_high);
+
+        std::unordered_map<int, bool> is_const;
+        std::unordered_set<int> unique_var_ids;
+
+        for (int i = 0; i < path_constraints.size(); i++) {
+            path_constraints[i].gather_var_ids(unique_var_ids);
+        }
+
+        for (int i : unique_var_ids) {
+            if (params.find(i) == params.end()) {
+                params.emplace(std::make_pair(i, dist(gen)));
+                is_const.emplace(std::make_pair(i, false));
+            } else {
+                is_const.emplace(std::make_pair(i, is_init_params_const));
             }
-            params.at(g.first) -= lr * sign;
-          }
         }
-      }
-      is_sat = eval(path_constraints, params);
-      itr++;
-      num_used_itr++;
+
+        int itr = 0;
+        bool is_sat = eval(path_constraints, params);
+
+        while ((!is_sat) && (itr < num_epochs)) {
+            Grad grads = Grad({});
+            for (int i = 0; i < path_constraints.size(); i++) {
+                if (path_constraints[i].eval(params, eps) > 0.0f) {
+                    grads = grads + path_constraints[i].grad(params, eps);
+                }
+            }
+            for (auto &g : grads.val) {
+                if (!is_const.at(g.first)) {
+                    if (!sign_grad) {
+                        params.at(g.first) -= lr * g.second;
+                    } else {
+                        float sign = 0.0f;
+                        if (g.second > 0.0f) {
+                            sign = 1.0;
+                        } else if (g.second < 0.0f) {
+                            sign = -1.0f;
+                        }
+                        params.at(g.first) -= lr * sign;
+                    }
+                }
+            }
+            is_sat = eval(path_constraints, params);
+            itr++;
+            num_used_itr++;
+        }
+        return is_sat;
     }
-    return is_sat;
-  }
 };
-} // namespace gymbo
+}  // namespace gymbo
--- libgymbo/tokenizer.h
+++ libgymbo/tokenizer.h
@@ -1,71 +1,72 @@
 #pragma once
-#include <cstdlib>
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unordered_map>
-#include <iostream>
+
+#include <cstdlib>
 #include <cstring>
+#include <iostream>
+#include <unordered_map>
 
 namespace gymbo {
 inline bool is_alpha(char c) {
-  return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+    return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
 }
 
 inline bool is_alnum(char c) { return is_alpha(c) || ('0' <= c && c <= '9'); }
 
 // Reports an error and exit.
 inline void error(char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
 }
 
 // Reports an error location and exit.
 inline void error_at(char *user_input, char *loc, char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
+    va_list ap;
+    va_start(ap, fmt);
 
-  int pos = loc - user_input;
-  fprintf(stderr, "%s\n", user_input);
-  fprintf(stderr, "%*s", pos, ""); // print pos spaces.
-  fprintf(stderr, "^ ");
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");  // print pos spaces.
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
 }
 
 typedef enum {
-  TOKEN_RESERVED, // Keywords or punctuators
-  TOKEN_RETURN,
-  TOKEN_IF,
-  TOKEN_ELSE,
-  TOKEN_FOR,
-  TOKEN_IDENT,
-  TOKEN_NUM, // Integer literals
-  TOKEN_EOF, // End-of-file markers
+    TOKEN_RESERVED,  // Keywords or punctuators
+    TOKEN_RETURN,
+    TOKEN_IF,
+    TOKEN_ELSE,
+    TOKEN_FOR,
+    TOKEN_IDENT,
+    TOKEN_NUM,  // Integer literals
+    TOKEN_EOF,  // End-of-file markers
 } TokenKind;
 
 typedef struct Token Token;
 struct Token {
-  TokenKind kind; // Token kind
-  Token *next;    // Next token
-  float val;     // If kind is TOKEN_NUM, its value
-  char *str;      // Token string
-  int len;        // Token length
-  int var_id;      // Var ID 
+    TokenKind kind;  // Token kind
+    Token *next;     // Next token
+    float val;       // If kind is TOKEN_NUM, its value
+    char *str;       // Token string
+    int len;         // Token length
+    int var_id;      // Var ID
 };
 
 struct LVar {
-  LVar *next;
-  char *name;
-  int len;
-  int offset;
+    LVar *next;
+    char *name;
+    int len;
+    int offset;
 };
 
 /**
@@ -76,11 +77,11 @@ struct LVar {
  * @return True if the token was consumed, false otherwise.
  */
 inline bool consume(Token *&token, char *op) {
-  if (token->kind != TOKEN_RESERVED || strlen(op) != token->len ||
-      memcmp(token->str, op, token->len))
-    return false;
-  token = token->next;
-  return true;
+    if (token->kind != TOKEN_RESERVED || strlen(op) != token->len ||
+        memcmp(token->str, op, token->len))
+        return false;
+    token = token->next;
+    return true;
 }
 
 /**
@@ -91,11 +92,11 @@ inline bool consume(Token *&token, char *op) {
  * @return True if the token was consumed, false otherwise.
  */
 inline bool consume_tok(Token *&token, TokenKind tok) {
-  if (token->kind != tok) {
-    return false;
-  }
-  token = token->next;
-  return true;
+    if (token->kind != tok) {
+        return false;
+    }
+    token = token->next;
+    return true;
 }
 
 /**
@@ -106,11 +107,10 @@ inline bool consume_tok(Token *&token, TokenKind tok) {
  * token is not an identifier.
  */
 inline Token *consume_ident(Token *&token) {
-  if (token->kind != TOKEN_IDENT)
-    return NULL;
-  Token *t = token;
-  token = token->next;
-  return t;
+    if (token->kind != TOKEN_IDENT) return NULL;
+    Token *t = token;
+    token = token->next;
+    return t;
 }
 
 /**
@@ -122,12 +122,12 @@ inline Token *consume_ident(Token *&token) {
  * @throws An error if the current token does not match `op`.
  */
 inline void expect(Token *&token, char *user_input, char *op) {
-  if (token->kind != TOKEN_RESERVED || strlen(op) != token->len ||
-      memcmp(token->str, op, token->len)) {
-    char em[] = "expected \"%s\"";
-    error_at(user_input, token->str, em, op);
-  }
-  token = token->next;
+    if (token->kind != TOKEN_RESERVED || strlen(op) != token->len ||
+        memcmp(token->str, op, token->len)) {
+        char em[] = "expected \"%s\"";
+        error_at(user_input, token->str, em, op);
+    }
+    token = token->next;
 }
 
 /**
@@ -138,13 +138,13 @@ inline void expect(Token *&token, char *user_input, char *op) {
  * @throws An error if the current token is not a number.
  */
 inline float expect_number(Token *&token, char *user_input) {
-  if (token->kind != TOKEN_NUM) {
-    char em[] = "expected a number";
-    error_at(user_input, token->str, em);
-  }
-  float val = token->val;
-  token = token->next;
-  return val;
+    if (token->kind != TOKEN_NUM) {
+        char em[] = "expected a number";
+        error_at(user_input, token->str, em);
+    }
+    float val = token->val;
+    token = token->next;
+    return val;
 }
 
 /**
@@ -166,12 +166,12 @@ inline bool at_eof(Token *token) { return token->kind == TOKEN_EOF; }
  * @return A pointer to the newly created token.
  */
 inline Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
-  Token *tok = (Token *)std::calloc(1, sizeof(Token));
-  tok->kind = kind;
-  tok->str = str;
-  tok->len = len;
-  cur->next = tok;
-  return tok;
+    Token *tok = (Token *)std::calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    tok->len = len;
+    cur->next = tok;
+    return tok;
 }
 
 /**
@@ -182,7 +182,7 @@ inline Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
  * @return True if `p` starts with `q`, false otherwise.
  */
 inline bool startswith(char *p, char *q) {
-  return memcmp(p, q, strlen(q)) == 0;
+    return memcmp(p, q, strlen(q)) == 0;
 }
 
 /**
@@ -192,103 +192,102 @@ inline bool startswith(char *p, char *q) {
  * @return A linked list of tokens.
  */
 inline Token *tokenize(char *user_input) {
-  char *p = user_input;
-  Token head;
-  head.next = NULL;
-  Token *cur = &head;
+    char *p = user_input;
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
 
-  char LETTER_EQ[] = "==";
-  char LETTER_NEQ[] = "!=";
-  char LETTER_LEQ[] = "<=";
-  char LETTER_GEQ[] = ">=";
-  char LETTER_AND[] = "&&";
-  char LETTER_OR[] = "||";
+    char LETTER_EQ[] = "==";
+    char LETTER_NEQ[] = "!=";
+    char LETTER_LEQ[] = "<=";
+    char LETTER_GEQ[] = ">=";
+    char LETTER_AND[] = "&&";
+    char LETTER_OR[] = "||";
 
-  std::unordered_map<std::string, int> var_counter;  
+    std::unordered_map<std::string, int> var_counter;
 
-  while (*p) {
-    // Skip whitespace characters.
-    if (isspace(*p)) {
-      p++;
-      continue;
+    while (*p) {
+        // Skip whitespace characters.
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        // Multi-letter punctuator
+        if (startswith(p, LETTER_EQ) || startswith(p, LETTER_NEQ) ||
+            startswith(p, LETTER_LEQ) || startswith(p, LETTER_GEQ) ||
+            startswith(p, LETTER_AND) || startswith(p, LETTER_OR)) {
+            cur = new_token(TOKEN_RESERVED, cur, p, 2);
+            p += 2;
+            continue;
+        }
+
+        if (strncmp(p, "if", 2) == 0 && !is_alnum(p[2])) {
+            cur = new_token(TOKEN_IF, cur, p, 2);
+            p += 2;
+            continue;
+        }
+
+        if (strncmp(p, "else", 4) == 0 && !is_alnum(p[4])) {
+            cur = new_token(TOKEN_ELSE, cur, p, 4);
+            p += 4;
+            continue;
+        }
+
+        if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
+            cur = new_token(TOKEN_RETURN, cur, p, 6);
+            p += 6;
+            continue;
+        }
+
+        // Single-letter punctuator
+        if (strchr("+-*/()<>=;{}", *p)) {
+            cur = new_token(TOKEN_RESERVED, cur, p++, 1);
+            continue;
+        }
+
+        // Numerical literal
+        if (isdigit(*p) || (*p == '.' && isdigit(p[1]))) {
+            cur = new_token(TOKEN_NUM, cur, p, 0);
+            char *q = p;
+            cur->val = strtof(p, &p);
+            cur->len = p - q;
+            continue;
+        }
+
+        // Variables
+        if (is_alpha(*p)) {
+            char *q = p;
+            while (is_alnum(*p)) p++;
+
+            char var_name[(p - q) + 1];
+            strncpy(var_name, q, (p - q));
+            var_name[p - q] = '\0';
+            std::string var_name_s(var_name);
+
+            if (var_counter.find(var_name_s) == var_counter.end()) {
+                var_counter.emplace(var_name_s, (int)var_counter.size());
+            }
+
+            cur = new_token(TOKEN_IDENT, cur, q, p - q);
+            cur->var_id = var_counter[var_name_s];
+
+            continue;
+        }
+
+        /*
+        if ('a' <= *p && *p <= 'z') {
+          cur = new_token(TOKEN_IDENT, cur, p++, 0);
+          cur->len = 1;
+          continue;
+        }
+        */
+
+        char em[] = "invalid token\n";
+        error_at(user_input, p, em);
     }
 
-    // Multi-letter punctuator
-    if (startswith(p, LETTER_EQ) || startswith(p, LETTER_NEQ) ||
-        startswith(p, LETTER_LEQ) || startswith(p, LETTER_GEQ) ||
-        startswith(p, LETTER_AND) || startswith(p, LETTER_OR)) {
-      cur = new_token(TOKEN_RESERVED, cur, p, 2);
-      p += 2;
-      continue;
-    }
-
-    if (strncmp(p, "if", 2) == 0 && !is_alnum(p[2])) {
-      cur = new_token(TOKEN_IF, cur, p, 2);
-      p += 2;
-      continue;
-    }
-
-    if (strncmp(p, "else", 4) == 0 && !is_alnum(p[4])) {
-      cur = new_token(TOKEN_ELSE, cur, p, 4);
-      p += 4;
-      continue;
-    }
-
-    if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
-      cur = new_token(TOKEN_RETURN, cur, p, 6);
-      p += 6;
-      continue;
-    }
-
-    // Single-letter punctuator
-    if (strchr("+-*/()<>=;{}", *p)) {
-      cur = new_token(TOKEN_RESERVED, cur, p++, 1);
-      continue;
-    }
-
-    // Numerical literal
-    if (isdigit(*p) || (*p == '.' && isdigit(p[1]))) {
-      cur = new_token(TOKEN_NUM, cur, p, 0);
-      char *q = p;
-      cur->val = strtof(p, &p);
-      cur->len = p - q;
-      continue;
-    }
-
-    // Variables
-    if (is_alpha(*p)) {
-      char *q = p;
-      while (is_alnum(*p))
-        p++;
-
-      char var_name[(p - q) + 1];
-      strncpy(var_name, q, (p - q));
-      var_name[p - q] = '\0';
-      std::string var_name_s(var_name);
-
-      if (var_counter.find(var_name_s) == var_counter.end()) {
-        var_counter.emplace(var_name_s, (int)var_counter.size());
-      }
-
-      cur = new_token(TOKEN_IDENT, cur, q, p - q);
-      cur->var_id = var_counter[var_name_s];  
-
-      continue;
-    }
-
-    /*
-    if ('a' <= *p && *p <= 'z') {
-      cur = new_token(TOKEN_IDENT, cur, p++, 0);
-      cur->len = 1;
-      continue;
-    }
-    */
-
-    char em[] = "invalid token\n";
-    error_at(user_input, p, em);
-  }
-
-  new_token(TOKEN_EOF, cur, p, 0);
-  return head.next;
+    new_token(TOKEN_EOF, cur, p, 0);
+    return head.next;
 }
-} // namespace gymbo
+}  // namespace gymbo
