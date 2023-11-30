2d63afd0f449c9357e0beb677f95065dd850acbf
Koukyosyumei <koukyosyumei@hotmail.com>
2023-11-30 10:48:31 +0900

    update

--- example/nn.py
+++ example/nn.py
@@ -25,6 +25,7 @@ use_dpll = False
 if __name__ == "__main__":
     random.seed(42)
 
+    # Prepate Dataset
     X, y = make_classification(
         n_samples=100, random_state=1, n_features=10, n_informative=3, n_classes=3
     )
@@ -32,11 +33,17 @@ if __name__ == "__main__":
         X, y, stratify=y, random_state=1
     )
 
+    # Train NN Model
     clf = MLPClassifier(
         hidden_layer_sizes=(2), activation="relu", random_state=1, max_iter=100
     )
     clf.fit(X_train, y_train)
 
+    # Convert NN to a c-like program
+    feature_names = [f"sv_{j}" for j in range(X_train.shape[1])]
+    mlp_code = pmg.dump_sklearn_MLPClassifier(clf, feature_names)
+
+    # Prepare the condition that the adversarial example should satisfy
     param_low = X.min()
     param_high = X.max()
 
@@ -48,9 +55,6 @@ if __name__ == "__main__":
     y_origin = y[idx]
     y_pred = clf.predict(x_origin.reshape(1, -1)).item()
 
-    feature_names = [f"sv_{j}" for j in range(x_origin.shape[0])]
-
-    mlp_code = pmg.dump_sklearn_MLPClassifier(clf, feature_names)
     adv_condition = (
         "("
         + " || ".join(
@@ -73,6 +77,22 @@ if __name__ == "__main__":
         f"\nif ({adv_condition} && {perturbation_condition})\n return 1;\nreturn 0;"
     )
 
+    # Compile the program
+    var_counter, prg = plg.gcompile(mlp_code)
+
+    # Get the program counter of the target instrument
+    target_pc = 0
+    for i, instr in enumerate(prg):
+        if i > 0 and prg[i - 1].toString() == "jmp":
+            target_pc = i
+
+    # Set the concrete values to consts
+    init_symstate = plg.SymState()
+    for j in range(x_origin.shape[0]):
+        if j not in symbolic_vars_id:
+            init_symstate.set_concrete_val(var_counter[f"sv_{j}"], x_origin[j])
+
+    # Prepare the gradient-descent optimizer
     optimizer = plg.GDOptimizer(
         num_itrs,
         step_size,
@@ -84,19 +104,7 @@ if __name__ == "__main__":
         seed,
     )
 
-    var_counter, prg = plg.gcompile(mlp_code)
-
-    target_pc = 0
-    for i, instr in enumerate(prg):
-        if i > 0 and prg[i - 1].toString() == "jmp":
-            target_pc = i
-
-    init_symstate = plg.SymState()
-
-    for j in range(x_origin.shape[0]):
-        if j not in symbolic_vars_id:
-            init_symstate.set_concrete_val(var_counter[f"sv_{j}"], x_origin[j])
-
+    # Execute attack
     target_pcs = {target_pc}
     constraints = plg.gexecute(
         prg,
@@ -112,6 +120,7 @@ if __name__ == "__main__":
         verbose_level,
     )
 
+    # Check the performance of generated adversarial examples
     for j in range(len(constraints)):
         x_adv = x_origin.copy()
 
