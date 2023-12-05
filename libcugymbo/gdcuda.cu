aeee0139bf5f29257539ca1a10b8d319b0cc0d10
Koukyosyumei <koukyosyumei@hotmail.com>
2023-12-05 19:02:53 +0900

    update

--- libcugymbo/gdcuda.cu
+++ libcugymbo/gdcuda.cu
@@ -1,20 +1,18 @@
 #include "cuda_runtime.h"
 #include "gdcuda.h"
 
-__global__ void kernel_vecDouble(int *in, int *out, const int n)
-{
+__global__ void kernel_vecDouble(int *in, int *out, const int n) {
     int i = threadIdx.x;
     if (i < n) {
         out[i] = in[i] * 2;
     }
 }
 
-void vecDouble(int *hIn, int *hOut, const int n)
-{
+void vecDouble(int *hIn, int *hOut, const int n) {
     int *dIn;
     int *dOut;
-    cudaMallocHost((void**)&dIn, n * sizeof(int));
-    cudaMallocHost((void**)&dOut, n * sizeof(int));
+    cudaMallocHost((void **)&dIn, n * sizeof(int));
+    cudaMallocHost((void **)&dOut, n * sizeof(int));
     cudaMemcpy(dIn, hIn, n * sizeof(int), cudaMemcpyHostToDevice);
 
     kernel_vecDouble<<<1, n>>>(dIn, dOut, n);
@@ -24,3 +22,76 @@ void vecDouble(int *hIn, int *hOut, const int n)
     cudaFree(dIn);
     cudaFree(dOut);
 }
+
+__global__ void gradientDescentKernel(float *params,
+                                      const float *path_constraints,
+                                      int num_params, int num_constraints,
+                                      float lr, float eps, bool sign_grad) {
+    // Calculate thread and block indices
+    int tid = threadIdx.x + blockDim.x * blockIdx.x;
+
+    // Perform gradient descent optimization for a subset of parameters
+    while (tid < num_params) {
+        // Compute gradient and update parameters
+        // ...
+
+        tid += blockDim.x * gridDim.x;
+    }
+}
+
+namespace gymbo {
+
+bool GDOptimizerCUDA::solve(std::vector<Sym> &path_constraints,
+                            std::unordered_map<int, float> &params,
+                            bool is_init_params_const) {
+    // ...
+
+    // Allocate GPU memory for path constraints
+    float *d_path_constraints;
+    cudaMalloc((void **)&d_path_constraints,
+               path_constraints.size() * sizeof(float));
+
+    // Copy path constraints from CPU to GPU
+    for (int i = 0; i < path_constraints.size(); ++i) {
+        float constraint_result = path_constraints[i].eval(params, eps);
+        cudaMemcpy(&d_path_constraints[i], &constraint_result, sizeof(float),
+                   cudaMemcpyHostToDevice);
+    }
+
+    num_params = params.size();
+    std::vector<float> h_params;
+    for (auto &p : params) {
+        h_params.emplace_back(p.second);
+    }
+
+    // Allocate GPU memory for parameters
+    float *d_params;
+    cudaMalloc((void **)&d_params, num_params * sizeof(float));
+
+    // Copy parameter values from CPU to GPU
+    cudaMemcpy(d_params, h_params, num_params * sizeof(float),
+               cudaMemcpyHostToDevice);
+
+    // Launch the CUDA kernel
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (num_params + threadsPerBlock - 1) / threadsPerBlock;
+    gradientDescentKernel<<<blocksPerGrid, threadsPerBlock>>>(
+        d_params, d_path_constraints, num_params, path_constraints.size(), lr,
+        eps, sign_grad);
+    cudaDeviceSynchronize();  // Wait for the kernel to finish
+
+    // Copy the results back from GPU to CPU
+    cudaMemcpy(h_params, d_params, num_params * sizeof(float),
+               cudaMemcpyDeviceToHost);
+
+    // Free GPU memory
+    cudaFree(d_params);
+    cudaFree(d_path_constraints);
+
+    // ...
+
+    return is_sat;
+}
+
+}  // namespace gymbo
+
