eaeca5938e430e0466f6040d1e0f6efaa592969b
Koukyosyumei <koukyosyumei@hotmail.com>
2023-12-05 18:58:08 +0900

    update

--- libcugymbo/gdcuda.h
+++ libcugymbo/gdcuda.h
@@ -1,6 +1,17 @@
+#pragma once
 #ifndef CUDA_VEC_DOUBLE_H
 #define CUDA_VEC_DOUBLE_H
 
+#include "../libgymbo/gd.h"
+
 void vecDouble(int *hIn, int *hOut, const int n);
 
-#endif  /* CUDA_VEC_DOUBLE_H */
+namespace gymbo {
+
+struct GDOptimizerCUDA : public GDOptimizer {
+    using GDOptimizer::GDOptimizer;
+};
+
+}  // namespace gymbo
+
+#endif /* CUDA_VEC_DOUBLE_H */
