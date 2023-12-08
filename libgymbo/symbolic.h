5fee5235d23b0c408cf346e088666ae4f3f301fb
Koukyosyumei <koukyosyumei@hotmail.com>
2023-12-08 21:01:33 +0900

    functionize SMT solvers

--- libgymbo/symbolic.h
+++ libgymbo/symbolic.h
@@ -5,8 +5,7 @@
  */
 
 #pragma once
-#include "gd.h"
-#include "sat.h"
+#include "smt.h"
 
 namespace gymbo {
 Trace run_gymbo(Prog &prog, GDOptimizer &optimizer, SymState &state,
@@ -16,27 +15,6 @@ Trace run_gymbo(Prog &prog, GDOptimizer &optimizer, SymState &state,
                 bool ignore_memory, bool use_dpll, int verbose_level,
                 bool return_trace);
 void symStep(SymState *state, Instr &instr, std::vector<SymState *> &);
-
-/**
- * @brief Initialize Parameter Values from Memory.
- *
- * This function initializes parameter values from memory, provided that
- * `ignore_memory` is set to false.
- *
- * @param params A map to store parameter values.
- * @param state The symbolic state of the program.
- * @param ignore_memory A flag indicating whether memory constraints should be
- * ignored.
- */
-void initialize_params(std::unordered_map<int, float> &params, SymState &state,
-                       bool ignore_memory) {
-    params = {};
-    if (!ignore_memory) {
-        for (auto &p : state.mem) {
-            params.emplace(std::make_pair(p.first, wordToFloat(p.second)));
-        }
-    }
-}
 
 /**
  * @brief Symbolically Execute a Program with Gradient Descent Optimization.
@@ -104,68 +82,12 @@ inline Trace run_gymbo(Prog &prog, GDOptimizer &optimizer, SymState &state,
             params = constraints_cache[constraints_str].second;
             is_unknown_path_constraint = false;
         } else {
-            std::unordered_map<std::string, gymbo::Sym *> unique_terms_map;
-            std::unordered_map<std::string, bool> assignments_map;
-            std::shared_ptr<gymbosat::Expr> path_constraints_expr =
-                gymbosat::pathconstraints2expr(state.path_constraints,
-                                               unique_terms_map);
-
             if (use_dpll) {
-                while (
-                    satisfiableDPLL(path_constraints_expr, assignments_map)) {
-                    std::vector<Sym> new_constraints;
-                    for (auto &ass : assignments_map) {
-                        if (ass.second) {
-                            new_constraints.emplace_back(
-                                *unique_terms_map[ass.first]);
-                        } else {
-                            new_constraints.emplace_back(Sym(
-                                SymType::SNot, unique_terms_map[ass.first]));
-                        }
-                    }
-
-                    for (int j = 0; j < max_num_trials; j++) {
-                        is_sat = optimizer.solve(new_constraints, params);
-                        if (is_sat) {
-                            break;
-                        }
-                        optimizer.seed += 1;
-                        initialize_params(params, state, ignore_memory);
-                    }
-
-                    if (is_sat) {
-                        break;
-                    }
-
-                    // add feedback
-                    std::shared_ptr<gymbosat::Expr> learnt_constraints =
-                        std::make_shared<gymbosat::Const>(false);
-                    for (auto &ass : assignments_map) {
-                        if (ass.second) {
-                            learnt_constraints = std::make_shared<gymbosat::Or>(
-                                learnt_constraints,
-                                std::make_shared<gymbosat::Not>(
-                                    std::make_shared<gymbosat::Var>(
-                                        ass.first)));
-                        } else {
-                            learnt_constraints = std::make_shared<gymbosat::Or>(
-                                learnt_constraints,
-                                std::make_shared<gymbosat::Var>(ass.first));
-                        }
-                    }
-
-                    path_constraints_expr = std::make_shared<gymbosat::And>(
-                        path_constraints_expr, learnt_constraints);
-                }
+                smt_dpll_solver(is_sat, state, params, optimizer,
+                                max_num_trials, ignore_memory);
             } else {
-                for (int j = 0; j < max_num_trials; j++) {
-                    is_sat = optimizer.solve(state.path_constraints, params);
-                    if (is_sat) {
-                        break;
-                    }
-                    optimizer.seed += 1;
-                    initialize_params(params, state, ignore_memory);
-                }
+                smt_union_solver(is_sat, state, params, optimizer,
+                                 max_num_trials, ignore_memory);
             }
             if (is_sat) {
                 maxSAT--;
